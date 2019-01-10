fa9be5f673714ebc8ab4d80d722943d801dc0740
tmknom <tmk.nom@gmail.com>
2019-01-10 11:33:50 +0900

    Update complete example to reflect support for create_ecs_task_execution_role and ecs_task_execution_role_arn

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -22,6 +22,9 @@ module "ecs_scheduled_task" {
   create_ecs_events_role = false
   ecs_events_role_arn    = "${aws_iam_role.ecs_events.arn}"
 
+  create_ecs_task_execution_role = false
+  ecs_task_execution_role_arn    = "${aws_iam_role.ecs_task_execution.arn}"
+
   tags = {
     Environment = "prod"
   }
@@ -57,6 +60,36 @@ resource "aws_iam_role_policy_attachment" "ecs_events" {
   policy_arn = "${aws_iam_policy.ecs_events.arn}"
 }
 
+resource "aws_iam_role" "ecs_task_execution" {
+  name               = "ecs-task-execution-for-ecs-scheduled-task"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "ecs_task_execution" {
+  name   = "${aws_iam_role.ecs_task_execution.name}"
+  policy = "${data.aws_iam_policy.ecs_task_execution.policy}"
+}
+
+data "aws_iam_policy" "ecs_task_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = "${aws_iam_role.ecs_task_execution.name}"
+  policy_arn = "${aws_iam_policy.ecs_task_execution.arn}"
+}
+
 data "template_file" "default" {
   template = "${file("${path.module}/container_definitions.json")}"
 
@@ -77,10 +110,6 @@ locals {
 
 resource "aws_ecs_cluster" "example" {
   name = "ecs-scheduled-task"
-}
-
-data "aws_iam_policy" "ecs_task_execution" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 module "vpc" {
